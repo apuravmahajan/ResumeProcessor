f5a850078f2f2dc7cdaddf69050e0222fbd391ea
Apurav Mahajan <mahajanapurav991@gmail.com>
2026-07-02 12:32:48 +0530

    Rename first_name to full_name in Candidate model

--- resume/models.py
+++ resume/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Candidate model
 class Candidate(models.Model):
-    first_name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=100)
     email = models.EmailField()
     mobile_number = models.CharField(max_length=15)
     
